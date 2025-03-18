dad544373b55a877cf9f080fbc04d7877b4a1db9
N0ViP <01.xx.vip@gmail.com>
2025-03-18 12:05:40 +0000

    delete all tests

--- ft_exit.c
+++
@@ -1,9 +0,0 @@
-#include "minitalk.h"
-
-
-/*this fct is uncompleted*/
-
-void ft_exit(int n)
-{
-	exit(n);
-}
--- ft_isspace.c
+++
@@ -1,6 +0,0 @@
-# include "minitalk.h"
-
-int ft_isspace(int c)
-{
-    return ((c >= 9 && c <= 13) || c == 31);
-}
