aeb7c102a7eab9a408a91062435732a19c7e345e
N0ViP <01.xx.vip@gmail.com>
2025-03-20 06:32:18 +0000

    finish client

--- client.c
+++ client.c
@@ -36,33 +36,31 @@ static void	signal_handler(int	signum, siginfo_t *info, void __attribute__((unus
 	}
 }
 
-static	void	send_byte(char c, int server_pid)
+static	void	send_byte(char *s, int server_pid)
 {
 	int	bits;
+	int	sig;
 
-	bits = 7;
-	while (bits >= 0)
+	while (*s)
 	{
-		if (((c >> bits) & 0X01) == 1)
+		bits = 7;
+		while (bits >= 0)
 		{
-			if (kill(server_pid, SIGUSR1) == -1)
+			if (((*s >> bits) & 0X01) == 1)
+				sig = SIGUSR1;
+			else
+				sig = SIGUSR2;
+			if (kill(server_pid, sig) == -1)
 			{
 				write(1, "kill failed\n", 13);
 				exit(1);
 			}
+			while (!g_var)
+				usleep(10);
+			g_var = 0;
+			bits--;
 		}
-		else
-		{
-			if (kill(server_pid, SIGUSR2) == -1)
-			{
-				write(1, "kill failed\n", 13);
-				exit(1);
-			}
-		}
-		while (!g_var)
-			usleep(50);
-		g_var = 0;
-		bits--;
+		s++;
 	}
 }
 
@@ -70,27 +68,20 @@ int	main(int ac, char *av[])
 {
 	struct sigaction	act;
 	int					server_pid;			
-	int					i;
 
 	if (ac != 3)
 		return (write(1, "Usage: ./client <server_pid> \"message\"\n", 40), 1);
 	server_pid = ft_ascii_to_int(av[1]);
-	if (server_pid <= 0)
+	if (server_pid <= 0 || kill(server_pid, 0) == -1)
 		return (write(1, "Invalid server PID\n", 20), 1);
 	act.sa_flags = SA_SIGINFO;
 	act.sa_sigaction = signal_handler;
 	if (sigemptyset(&act.sa_mask) == -1)
 		return (write(1, "sigemptyset failed\n", 20), 1);
-	if (sigaction(SIGUSR1, &act, NULL) == -1)
+	if (sigaction(SIGUSR1, &act, NULL) == -1
+		|| sigaction(SIGUSR2, &act, NULL) == -1)
 		return (write(1, "sigaction failed\n", 18), 1);
-	if (sigaction(SIGUSR2, &act, NULL) == -1)
-		return (write(1, "sigaction failed\n", 18), 1);
-	i = 0;
-	while (av[2][i])
-	{
-		send_byte(av[2][i++], server_pid);
-	}
-	send_byte(0, server_pid);
+	send_byte(av[2], server_pid);
 	while (1)
 		pause();
 }
